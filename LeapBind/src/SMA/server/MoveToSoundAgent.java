2ba30f3aba46f06b3e83b87c10cc7a2a98699bc5
Alex6092 <alexandre.gremont@gmail.com>
2014-05-20 13:03:41 +0200

    Nouvelles structure de donnees et structure de message, nouveaux behaviour, agent SoundPlayAgent

--- LeapBind/src/SMA/server/MoveToSoundAgent.java
+++ LeapBind/src/SMA/server/MoveToSoundAgent.java
@@ -9,10 +9,13 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import Utilities.MoveInformData;
 import Utilities.NoteInformData;
+import Utilities.NoteInformData.NoteActionType;
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.Timer;
@@ -24,41 +27,9 @@ import jade.lang.acl.MessageTemplate;
 
 
 public class MoveToSoundAgent extends Agent{
-	private String current_note;
-	private String current_instrument;
-	private NoteInformData inform_note;
-	private NoteInformData inform_note1;
-	private NoteInformData inform_note2;
-	private NoteInformData inform_note3;
-	private NoteInformData inform_note4;
-	private NoteInformData inform_note5;
-	private NoteInformData inform_note6;
-	private NoteInformData inform_note7;
-	private NoteInformData inform_note8;
-	private ArrayList<NoteInformData> noteslist;
+	
 	protected void setup() {
 		
-		//CDEFGAB 
-		inform_note = new NoteInformData("C5W", "Guitar",1);
-		inform_note1 = new NoteInformData("D5W", "Guitar",1);
-		inform_note2 = new NoteInformData("E5W", "Piano",1);
-		inform_note3 = new NoteInformData("F5W", "Piano",1);
-		inform_note4 = new NoteInformData("G5W", "Ocarina",1);
-		inform_note5 = new NoteInformData("[Crash_Cymbal_1]q", "Percussion",1);
-		inform_note6 = new NoteInformData("[Hand_Clap]q", "Percussion",1);
-		inform_note7 = new NoteInformData("[COWBELL]q", "Percussion",1);
-		//inform_note7 = new NoteInformData("G5W", "Ocarina",1);
-		inform_note8 = new NoteInformData("A5W", "Ocarina",1);
-		noteslist=new ArrayList<NoteInformData>();
-		noteslist.add(inform_note1);
-		noteslist.add(inform_note2);
-		noteslist.add(inform_note3);
-		noteslist.add(inform_note4);
-		noteslist.add(inform_note5);
-		noteslist.add(inform_note6);
-		noteslist.add(inform_note7);
-		noteslist.add(inform_note8);
-		noteslist.add(inform_note);
 		
 		this.addBehaviour(new SoundMessageDaemonBehaviour() );	
 		
@@ -73,24 +44,40 @@ public class MoveToSoundAgent extends Agent{
 			
 			if(message != null)
 			{
-				
+					ObjectMapper mapper = new ObjectMapper();
+					try {
+						MoveInformData moveData = mapper.readValue(message.getContent(), MoveInformData.class);
+						
+						switch(moveData.getInstrumentType()) {
+							case TAMBOUR:
+								break;
+							case PIANO:
+								break;
+							case GUITAR:
+								break;
+						}
+						
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
 					
 
 					//AID aid = (AID)message.getAllReplyTo().next();
+					/*
 					AID aid=new AID("SoundPlayer", AID.ISLOCALNAME);
-					int index =(int) (Math.random()*9);
-					addBehaviour(new SenderInformBehaviour(noteslist.get(index), aid));
-					//addBehaviour(new SenderInformBehaviour(inform_note2, aid));
-					//addBehaviour(new SenderInformBehaviour(inform_note3, aid));
-					//addBehaviour(new SenderInformBehaviour(inform_note4, aid));
-					//addBehaviour(new SenderInformBehaviour(inform_note5, aid));
-					//addBehaviour(new SenderInformBehaviour(inform_note6, aid));
-					//addBehaviour(new SenderInformBehaviour(inform_note7, aid));
-					//addBehaviour(new SenderInformBehaviour(inform_note8, aid));
-					 
+					int index =(int) ((Math.random()*9)+60);
 					
-
+					NoteInformData data = new NoteInformData();
+					data.setAction(NoteActionType.START_NOTE);
+					data.setVelocity(255);
+					data.setChannel(0);
+					data.setNote(index);
+					
+					addBehaviour(new SenderInformBehaviour(data, aid));
+					*/
+				
 			}
+			else block();
 		}	
 	}
 
@@ -135,7 +122,6 @@ public class MoveToSoundAgent extends Agent{
 	
 	
 	public class SenderInformBehaviour extends OneShotBehaviour {
-		private boolean Done;
 		private AID aid;
 		NoteInformData inform;
 		private UUID id;
@@ -145,7 +131,6 @@ public class MoveToSoundAgent extends Agent{
 			this.aid=aid;
 			this.inform=inform;
 			this.id = UUID.randomUUID();
-			Done = false;
 		}
 
 		@Override
--- LeapBind/src/Utilities/MoveInformData.java
+++ LeapBind/src/Utilities/MoveInformData.java
@@ -1,5 +1,34 @@
 package Utilities;
-///From User to MoveToSound message structure
+
+import jade.core.AID;
+
 public class MoveInformData {
-//instrument speed...
+	private Movement move;
+	private AID host_AID;
+	private InstrumentType instrumentType;
+	
+	public MoveInformData() {
+		move = null;
+		host_AID = null;
+		instrumentType = InstrumentType.DEFAULT;
+	}
+	
+	public Movement getMove() {
+		return move;
+	}
+	public void setMove(Movement move) {
+		this.move = move;
+	}
+	public AID getHost_AID() {
+		return host_AID;
+	}
+	public void setHost_AID(AID host_AID) {
+		this.host_AID = host_AID;
+	}
+	public InstrumentType getInstrumentType() {
+		return instrumentType;
+	}
+	public void setInstrumentType(InstrumentType instrumentType) {
+		this.instrumentType = instrumentType;
+	}
 }
