e3a1a183c877940d176dccd1c1c8587a029fde32
kewei <keweihu@yahoo.com>
2014-06-09 22:45:06 +0200

    FIX::warnning

---
+++ src/fr/utc/leapband/sma/move/FindNoteFromMovement.java
@@ -0,0 +1,42 @@
+package fr.utc.leapband.sma.move;
+
+import jade.core.behaviours.OneShotBehaviour;
+import fr.utc.leapband.utilities.Movement;
+
+@SuppressWarnings("serial")
+public abstract class FindNoteFromMovement extends OneShotBehaviour {
+	protected Movement movement;
+	protected int note;
+	// speed 400-2000
+
+	
+	public FindNoteFromMovement(Movement movement) {
+		this.movement = movement;
+	}
+	
+	public int matchNote() {
+		//TODO
+		return note;
+
+	}
+	protected int matchVolume() {
+		int volume = 80;
+
+		volume = (int) (volume + (movement.getSpeed()-1200)/80);
+		if (volume<60){
+			volume=60;
+		}
+		if (volume>127){
+			volume=127;
+		}
+		if (movement.getSpeed() <50){
+			volume=0;
+		}
+		
+		return volume;
+	}
+		
+	
+	
+
+}
